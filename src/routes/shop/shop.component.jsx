6ca057ec9868dc3e891a5e30278cd6243051d97a
Shay.Asanova <shay.asanova@gmail.com>
2023-03-13 16:39:18 +0400

    implemented redux toolkit with categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,11 +4,11 @@ import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.reducer";
 
 const Shop = () => {
   const dispatch = useDispatch();
-  //USED THIS EFFECT TO ADD SHOP DATA TO FIRESTORE.
+  //USED THIS EFFECT TO ADD SHOP_DATA TO FIRESTORE.
   // useEffect(() => {
   //   addCollectionAndDocuments("categories", SHOP_DATA);
   // }, []);
