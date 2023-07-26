a4cd8433512a446c2531400126003d31099b03c2
shay asanova <shay29@shays-MacBook-Air.local>
2023-07-26 15:07:45 +0400

    added a redirect/navigate to shop page if user is successufully signed in feature

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,9 +1,23 @@
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
-
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { AuthenticationContainer } from './authentication.styles';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 const Authentication = () => {
+ 
+const currentUser = useSelector(selectCurrentUser)
+const navigate = useNavigate();
+
+useEffect(()=>{
+  if(currentUser){
+    navigate('/shop')
+  }else{
+    console.log('this didnt work')
+  }
+},[currentUser, navigate])
   return (
     <AuthenticationContainer>
       <SignInForm />
